04d4b87c6b4b6e9a3d9d199859eca64502135f3c
sal <ximena.salgadouribe@epfl.ch>
2023-01-12 16:07:41 +0100

    feat: added api

--- config/api_router.py
+++ config/api_router.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from django_storage_test.users.api.views import UserViewSet
+from django_storage_test.videos.api.views import VideoViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -9,7 +10,7 @@ else:
     router = SimpleRouter()
 
 router.register("users", UserViewSet)
-
+router.register("videos", VideoViewSet)
 
 app_name = "api"
 urlpatterns = router.urls
