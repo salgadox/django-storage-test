d524e43b9e0fdd7c091e2b1f786704693e4a6c54
sal <ximena.salgadouribe@epfl.ch>
2023-01-28 17:27:08 +0100

    fix: donwload button goes to new view

--- django_storage_test/files/urls.py
+++ django_storage_test/files/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from django_storage_test.files.views import FileDetailView, FileListView
+from django_storage_test.files.views import (
+    FileDetailView,
+    FileListView,
+    latest_file_view,
+)
 
 app_name = "files"
 urlpatterns = [
@@ -10,4 +14,5 @@ urlpatterns = [
         "upload/", TemplateView.as_view(template_name="file_upload.html"), name="upload"
     ),
     path("<str:file_name>/", FileDetailView.as_view(), name="detail"),
+    path("latest-file", latest_file_view, name="latest-file"),
 ]
